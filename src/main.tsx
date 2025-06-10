659535a276ec76f04f4b141442b40a0fe04c853c
leoxeom <marchoooleo@gmail.com>
2025-06-10 04:11:14 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App_auth from './App_auth.tsx';
+import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App_auth />
+    <App />
   </StrictMode>
 );
